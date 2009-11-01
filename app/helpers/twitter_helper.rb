4a56787f90c398c86d27cd56bebe23a195fbb04a
bugcloud <bugcloudisco@bugcloud.com>
2009-11-01 19:11:57 +0900

    Add helper adding link to usrname

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -3,8 +3,12 @@ module TwitterHelper
     a_forward = '<a href="'
     a_backward = '" target="_blank">'
     links = text.scan(/http[s]?\:\/\/[\w\+\$\;\?\.\%\,\!\#\~\*\/\:\@\&\\\=\_\-]+/)
+    usernames = text.scan(/@[a-zA-Z0-9_]+/).uniq
     links.each do |l|
       text = text.sub(l,a_forward + l + a_backward + l + "</a>")
+    end
+    usernames.each do |u|
+      text = text.gsub(u,a_forward + "http://twitter.com/" + u.sub("@","") + a_backward + u + "</a>")
     end
     return text
   end
