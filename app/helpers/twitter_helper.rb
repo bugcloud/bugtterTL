5231867e2e653812f371319dc6d53aaaa6552335
bugcloud <bugcloudisco@bugcloud.com>
2009-11-01 18:45:01 +0900

    Add helper adding <a href ~~>

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,2 +1,11 @@
 module TwitterHelper
+  def add_href(text)
+    a_forward = '<a href="'
+    a_backward = '" target="_blank">'
+    links = text.scan(/http[s]?\:\/\/[\w\+\$\;\?\.\%\,\!\#\~\*\/\:\@\&\\\=\_\-]+/)
+    links.each do |l|
+      text = text.sub(l,a_forward + l + a_backward + l + "</a>")
+    end
+    return text
+  end
 end
